71e0f727ba6bf00ac4be9815744728c016ed445e
Damon <dmarcr1997@gmail.com>
2022-03-25 14:59:00 -0400

    Created and Styled Punk List Component

--- mossy-cppunk/src/App.js
+++ mossy-cppunk/src/App.js
@@ -1,12 +1,24 @@
 import Header from "./components/Header"
 import './App.css';
-import CollectionCard from "./components/CollectionCard";
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+import PunkList from "./components/PunkList";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([])
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get("https://testnets-api.opensea.io/assets?asset_contract_address=0x31D6a54Fe07FF0713a84274A416060Dd5a09eE77&order_direction=asc")
+      console.log(openseaData.data.assets)
+      setPunkListData(openseaData.data.assets)
+    }
+    return getMyNfts()
+  }, [])
   return (
     <div className="app">
       <Header />
-      <CollectionCard id={0} name={"Bandana Punk"} traits={[{'value': 7}]} image='https://ipfs.thirdweb.com/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg'/>
+      <PunkList punkListData={punkListData} />
     </div>
   );
 }
