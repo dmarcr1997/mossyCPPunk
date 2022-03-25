4a92e7403f0892fe61b829fd1fd6146a1d0109b7
Damon <dmarcr1997@gmail.com>
2022-03-25 14:36:00 -0400

    Collection Card init

--- mossy-cppunk/src/App.js
+++ mossy-cppunk/src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
 import './App.css';
+import CollectionCard from "./components/CollectionCard";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard id={0} name={"Bandana Punk"} traits={[{'value': 7}]} image='https://ipfs.thirdweb.com/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg'/>
     </div>
   );
 }
